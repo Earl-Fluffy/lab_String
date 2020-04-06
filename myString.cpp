f0fa38d25f6d3fa7a923e1e28ca4c4aad07487b1
Julie <julie.kleines@gmail.com>
2020-04-06 15:38:30 +0200

    add a method affiche()

--- myString.cpp
+++ myString.cpp
@@ -1,5 +1,11 @@
 #include "myString.h"
 
-myString::myString(){	
+myString::myString(){
 	content = new char[10];
-}
+	content[0] = 'h';
+    content[1] = 'e';
+};
+
+char myString::affiche(){
+    return content[0];
+};
