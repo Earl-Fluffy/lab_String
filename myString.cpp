1ef987b7554877eb32738ef5cdacfed0f93a2cb0
Julie <julie.kleines@gmail.com>
2020-04-06 17:25:50 +0200

    add a new constructor

--- myString.cpp
+++ myString.cpp
@@ -10,6 +10,21 @@ myString::myString(){
 
 };
 
+myString::myString(const char* s){
+    int n;
+    n=0;
+    while (s[n]!= '\0'){
+        n=n+1;
+    }
+    std::cout << n << std::endl;
+    content = new char[n];
+
+    for (int i=0; i<n;++i){
+		content[i]=s[i];
+	};
+
+};
+
 char* myString::affiche(){
     return content;
 };
