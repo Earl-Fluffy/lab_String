36e1979d734c8368768cee5f8cfe5159c312105b
Julie <julie.kleines@gmail.com>
2020-04-06 15:38:56 +0200

    test le constructeur

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "myString.cpp"
+#include <iostream>
 
 int main(){
+    myString test;
+    char chaine[] = "hello world";
+    std::cout << "premier test : " << test.affiche() << std::endl;
 
+    std::cout << "avec char : " << chaine[0] << std::endl;
 	return 0;
 };
