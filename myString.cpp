21982b078568d55b03610c72fe983d78416644f3
Julie <julie.kleines@gmail.com>
2020-04-06 16:29:33 +0200

    change sizeof(temp) - 1

--- myString.cpp
+++ myString.cpp
@@ -4,10 +4,10 @@
 myString::myString(){
 	char temp[]="Hello World !";
 	content = new char[sizeof(temp)];
-	for (int i; sizeof(temp);++i){
+	for (int i; sizeof(temp)-1;++i){
 		content[i]=temp[i];
 	}
-	
+
 };
 
 char* myString::affiche(){
